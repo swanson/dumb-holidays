378221db482f8864dea9b0f319db89365afbab0f
Matt Swanson <matt@mdswanson.com>
2018-06-05 19:48:57 -0400

    Dont send on weekends

--- program.rb
+++ program.rb
@@ -1,10 +1,14 @@
 require 'dotenv/load'
 require_relative "./dumb_holidays"
 
-Slack.configure do |config|
-  config.token = ENV['SLACK_BOT_TOKEN']
+today = Time.now
+
+if !today.saturday? and !today.sunday?
+  Slack.configure do |config|
+    config.token = ENV['SLACK_BOT_TOKEN']
+  end
+
+  client = Slack::Web::Client.new
+
+  DumbHolidays.new(client, ENV['CHANNEL']).run!
 end
-
-client = Slack::Web::Client.new
-
-DumbHolidays.new(client, ENV['CHANNEL']).run!
