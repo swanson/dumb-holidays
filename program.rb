61f1083b3ff9328f2c2e52d28732e0e967b4da1b
matt swanson <matt@mdswanson.com>
2018-10-21 20:53:39 -0400

    Run every day

--- program.rb
+++ program.rb
@@ -3,14 +3,10 @@ Dotenv.load
 
 require_relative "./dumb_holidays"
 
-today = Time.now
-
-if !today.saturday? and !today.sunday?
-  Slack.configure do |config|
-    config.token = ENV['SLACK_BOT_TOKEN']
-  end
-
-  client = Slack::Web::Client.new
-
-  DumbHolidays.new(client, ENV['CHANNEL']).run!
+Slack.configure do |config|
+  config.token = ENV['SLACK_BOT_TOKEN']
 end
+
+client = Slack::Web::Client.new
+
+DumbHolidays.new(client, ENV['CHANNEL']).run!
